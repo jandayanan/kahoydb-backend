0e1e08e7bc70454c35ad71da1756397031729d45
Christian Miguel Dorado <doradomiguel35@gmail.com>
2023-02-28 14:40:00 +0800

    Miguel > Restore removed components, dependent for login page

---
+++ resources/js/Components/Pagination.vue
@@ -0,0 +1,30 @@
+<template>
+  <VPagination
+    v-model:currentPage="currentPage"
+    :total-pages="totalPages"
+    :boundary-links="true">
+  </VPagination>
+</template>
+
+<script>
+export default {
+  props: {
+    totalPages: {
+      type: Number,
+      default: 0
+    }
+  },
+  data(){
+    return {
+      currentPage: 1
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
