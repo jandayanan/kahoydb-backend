cece1e8e31e2059b80f14e6ec9cfd9cdfc553f88
Christian Miguel Dorado <doradomiguel35@gmail.com>
2023-03-23 20:05:30 +0800

    Miguel > Refactor carousel placement

--- resources/js/Layouts/PublicFaceLayout.vue
+++ resources/js/Layouts/PublicFaceLayout.vue
@@ -3,7 +3,30 @@
     <CRow>
       <p class="text-center text-uppercase text-white font-weight-bolder">Army 2040: World-Class. Multi-mission Ready. Cross-domain capable</p>
     </CRow>
-    <div class="d-flex justify-content-center">
+    <CRow>
+      <CCol md="4" class="d-flex justify-content-end justify-center-mobile mt-4">
+        <img src="@/Assets/placeholder.png" style="height: 16vh;" alt="">
+      </CCol>
+      <CCol md="4" class="d-flex justify-content-center">
+        <CRow>
+          <CCol md="6" class="mt-4 d-flex justify-content-center">
+            <img src="@/Assets/logo.png" style="height: 16vh;" alt="">
+          </CCol>
+          <CCol md="6" class="mt-4 d-flex justify-content-center">
+            <img src="@/Assets/doa_logo.png" style="height: 16vh;" alt="">
+          </CCol>
+        </CRow>
+        
+      </CCol>
+      <CCol md="4" class="text-center-mobile mt-4 d-flex justify-center-mobile" style="font-size: 1.6rem;">
+        <div>
+          <strong class="text-white text-uppercase">The Philippine Army</strong>
+          <p class="text-white my-0 font-weight-bolder">in partnership with</p>
+          <strong class="text-white text-uppercase">Department of Agriculture</strong>
+        </div>
+      </CCol>
+    </CRow>
+    <!-- <div class="d-flex justify-content-center">
       <CCol sm="6">
         <Carousel />
       </CCol>
@@ -14,7 +37,7 @@
         <p class="text-white my-0 font-weight-bolder">in partnership with</p>
         <strong class="text-white text-uppercase">Department of Agriculture</strong>
       </div>
-    </div>
+    </div> -->
   </CContainer>  
   <CContainer fluid class="px-0">
     <div class="banner">
@@ -30,6 +53,13 @@
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <h3 style="color: #FCDB02; font-size: 6rem;" class="my-0 h1-mobile text-center-mobile text-uppercase">Fruit-bearing Trees</h3>
       </div>
+    </div>
+  </CContainer>
+  <CContainer fluid class="py-5" style="background-color: #18583E;">
+    <div class="d-flex justify-content-center">
+      <CCol sm="6">
+        <Carousel />
+      </CCol>
     </div>
   </CContainer>
   <CContainer fluid style="background-color: #18583E;" class="p-4 d-flex justify-content-center block-mobile">
